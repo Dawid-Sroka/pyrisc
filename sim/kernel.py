99aff4d9d4b63b61965612fc4f6922dde793bfce
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2023-11-15 22:39:32 +0100

    first OS kernel: load one program, recognize EXC_EBREAK and EXC_CLOCK

--- sim/kernel.py
+++ sim/kernel.py
@@ -7,7 +7,6 @@ from program import *
 from sim import *
 from snurisc import *
 
-from threading import Timer
 
 # -- snurisc main
 filename = parse_args(sys.argv)
@@ -17,24 +16,28 @@ if not filename:
 
 cpu = SNURISC()
 prog = Program()
-entry_point = prog.load(cpu, filename)
+entry_point = prog.load(cpu, filename) # tu będziemy ładować init
 if not entry_point:
     sys.exit()
 # --
 
-# -- cpu clock
-def cpu_clock():
-    print("clock interrupt!")
-
-t = Timer(0.02, cpu_clock)
-t.start()
-# --
-
 for i in range(5):
-    cpu_ret = cpu.run(entry_point)
-    print(EXC_MSG[cpu_ret])
+#while(1):
+    print(i)
+    # zamiast wywoływać run pewnie chcemy najpierw wywołać jakieś set_context.
+    # I ta metoda będzie dopiero wywołać cpu.run
+    # Oprócz tego pewnie get_context i create_context
+    cpu_status = cpu.run(entry_point)
+    print(EXC_MSG[cpu_status])
     
-    if (cpu_ret == EXC_EBREAK):
+    if (cpu_status == EXC_EBREAK):
+        # Najpierw chcemy dodać proste syscalle. Najpierw exit
+        # Potem jakiś prosty read i write na standarwe wyjście i wejście
+        # zareaguj na syscalla
+        continue
+    elif (cpu_status == EXC_CLOCK):
+        # Patrzymy czy minął kwant czasu, czy zwykle przerwanie
+        # jakaś akcja schedulera
         continue
 
 
